d5e68cdf89c1f7638dc95efde43467617e0d8179
Nickolas Jensen <jensenn@student.psdr3.org>
2018-04-24 11:36:18 -0500

    World and tile added

---
+++ src/view/Tile.java
@@ -0,0 +1,26 @@
+package view;
+import java.awt.Color;
+
+public class Tile {
+	@SuppressWarnings("unused")
+	private int defMod, accMod, evdMod;
+	private String name;
+
+	private Color color;
+
+	public Tile(String name, Color color){
+		defMod = 0;
+		accMod = 0;
+		evdMod = 0;
+		this.color = color;
+		this.name = name;
+	}
+	public Tile(String name, int defMod, int accMod, int evdMod,Color color){
+		this.defMod = defMod;
+		this.accMod = accMod;
+		this.evdMod = evdMod;
+
+		this.color = color;
+		this.name=name;
+	}
+}
---
+++ src/view/World.java
@@ -0,0 +1,42 @@
+package view;
+
+import java.util.ArrayList;
+
+public class World {
+	private Tile[][] map;
+	private int size;
+	private String name;
+
+	public World(String name) {
+		this.name = name;
+		map = new Tile[10][10];
+		size = map.length;
+	}
+
+	public World(String name, int size) {
+		this.name = name;
+		this.size = size;
+		map = new Tile[size][size];
+		
+	}
+
+	public World(String name, Tile[][] map) {
+		this.name = name;
+		this.map = map;
+		size = map.length;
+	}
+
+	public Tile[][] generate(ArrayList<Tile> genList){
+		Tile[][] genMap = new Tile[size][size];
+		for(int i = 0; i < genList.size(); i++){
+			
+		}
+		return genMap;
+
+	}
+
+	public String toString(){
+		return String.format("",size);
+	}
+
+}
