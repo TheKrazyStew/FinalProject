5a2e3c17a47a6c8dea778a5d29a636e155c9a833
Nickolas Jensen <jensenn@student.psdr3.org>
2018-04-27 12:27:01 -0500

    I COPY PASTED MAPGUI

---
+++ src/view/MapGUI.java
@@ -0,0 +1,366 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.math.*;
+
+@SuppressWarnings("unused")
+public class MapGUI extends JPanel{
+
+	public static final Tile grassland = new Tile("grassland", new Color(41, 163, 41)), 
+			hills = new Tile("hills", new Color(31, 122, 31)),
+			woods = new Tile("woods", new Color(0, 153, 51)),
+			forest = new Tile("forest", new Color(0, 102, 34)),
+			valley = new Tile("valley", new Color(120, 120, 60)),
+			marsh = new Tile("marsh", new Color(119, 179, 0)),
+			sand = new Tile("desert", new Color(255, 255, 204)),
+			dunes = new Tile("dunes", new Color(224, 163, 61)),
+			quicksand = new Tile("quicksand", new Color(224, 224, 160)),
+			sValley = new Tile("valley", new Color(120, 120, 60)),
+			water = new Tile("river", new Color(51, 51, 204)),
+			road = new Tile("road", new Color(49, 52, 56)),
+			bridge = new Tile("bridge", new Color(98, 104, 102));
+
+
+	public static final int NUM_ROWS = 50, NUM_COLS = 50,PREFERRED_GRID_SIZE_PIXELS = 10;
+	public static final Tile[] 
+			TERRAIN = {grassland,grassland,grassland,grassland,hills,hills,hills,valley,woods,woods,forest,forest,forest,forest,marsh},
+			TERRAIN2 = {sand,sand,sand,sand,dunes,dunes,quicksand,sValley};
+
+	public static final Tile[][] WORLDGENERATORS = {TERRAIN,TERRAIN2};
+
+	private Tile[][] terrainGrid;
+
+	public MapGUI(){
+		
+		this.terrainGrid = new Tile[NUM_ROWS][NUM_COLS];
+		int r = (int)(Math.random()*(WORLDGENERATORS.length));
+		Tile[] selected = WORLDGENERATORS[r];
+		for (int i = 0; i < NUM_ROWS; i++) {
+
+			for (int j = 0; j < NUM_COLS; j++) {
+				int randomTerrainIndex = (int)(selected.length * Math.random());
+				Tile randomTile = selected[randomTerrainIndex];
+				this.terrainGrid[i][j] = randomTile; 
+			}
+
+		}
+		
+		this.terrainGrid = verticalRiverGenerator(terrainGrid);
+		this.terrainGrid = verticalRoadGenerator(terrainGrid);
+		this.terrainGrid = horizontalRoadGenerator(terrainGrid);
+		this.terrainGrid = horizontalRiverGenerator(terrainGrid);
+
+		int preferredWidth = NUM_COLS * PREFERRED_GRID_SIZE_PIXELS;
+		int preferredHeight = NUM_ROWS * PREFERRED_GRID_SIZE_PIXELS;
+		setPreferredSize(new Dimension(preferredWidth, preferredHeight));
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.clearRect(0, 0, getWidth(), getHeight());
+
+		int rectWidth = getWidth() / NUM_COLS;
+		int rectHeight = getHeight() / NUM_ROWS;
+
+		for (int i = 0; i < NUM_ROWS; i++) {
+			for (int j = 0; j < NUM_COLS; j++) {
+				int x = i * rectWidth;
+				int y = j * rectHeight;
+				Color terrainColor = terrainGrid[i][j].getColor();
+				g.setColor(terrainColor);
+				g.fillRect(x, y, rectWidth, rectHeight);
+			}
+		}
+	}
+
+	private static Tile[][] horizontalRiverGenerator(Tile[][] terrainGrid){
+
+		int max = (int)(Math.random()*5) +3, min = (int)(Math.random()*-5)-3; 
+		int n = 1, row = (int)(Math.random()*terrainGrid[0].length);
+
+		boolean toggle=true;
+
+		for(int i =0; i < terrainGrid.length; i++){
+
+
+			if(row+n == row+max && toggle && row+n >= 0 && row+n < terrainGrid[i].length){
+				toggle = false;
+				row+=n;
+				n=0;
+
+			}else if(row+n == row+min && !toggle && row+n >= 0 && row+n < terrainGrid[i].length){
+				toggle = true;
+				row+=n;
+				n=0;
+
+			}else if(row + n <= row + max && toggle){
+				n++;
+
+			}else if(row + n >= row + min && !toggle){
+				n--;
+			}
+
+			if( row+n > 0 && row+n < terrainGrid[i].length-1 ){
+				if(terrainGrid[i][row+n].equals(road)){
+					terrainGrid[i][row+n] = bridge;
+				}else{
+					terrainGrid[i][row+n] = water;
+				}
+			}else if(row+n == 0 || row+n == terrainGrid[i].length-1){
+				if(terrainGrid[i][row+n].equals(road)){
+					terrainGrid[i][row+n] = bridge;
+				}else{
+					terrainGrid[i][row+n] = water;
+				}
+				if(toggle){
+					toggle=false;
+					row+=n;
+					n=0;
+				}else{
+					toggle=true;
+					row+=n;
+					n=0;
+				}
+
+			}else if(row+n < 0 || row+n > terrainGrid[i].length ){
+				if(toggle){
+					toggle=false;
+				}else{
+					toggle=true;
+				}
+			}
+		}
+
+		return terrainGrid;
+
+	}
+
+	private static Tile[][] verticalRiverGenerator(Tile[][] terrainGrid){
+
+		int max = (int)(Math.random()*5) +3, min = (int)(Math.random()*-5)-3;
+		int n = 1, col = (int)(Math.random()*terrainGrid.length);
+		boolean toggle=true;
+
+
+
+		for(int j = 0; j < terrainGrid[col].length; j++){
+			
+
+			if(col+n == col+max && toggle && col+n >= 0 && col+n < terrainGrid.length){
+				toggle = false;
+				col+=n;
+				n=0;
+
+			}else if(col+n == col+min && !toggle && col+n >= 0 && col+n < terrainGrid.length){
+				toggle = true;
+				col+=n;
+				n=0;
+
+			}else if(col + n <= col + max && toggle){
+				n++;
+
+			}else if(col + n >= col + min && !toggle){
+				n--;
+
+			}else{
+
+			}
+
+			if( col+n > 0 && col+n < terrainGrid.length-1 ){
+				if(terrainGrid[col+n][j].equals(road)){
+					terrainGrid[col+n][col+n] = bridge;
+				}else{
+					terrainGrid[col+n][j] = water;
+				}
+			}else if(col+n == 0 || col+n == terrainGrid.length-1){
+				if(terrainGrid[col+n][j].equals(road)){
+					terrainGrid[col+n][j] = bridge;
+				}else{
+					terrainGrid[col+n][j] = water;
+				}
+				if(toggle){
+					toggle=false;
+					col+=n;
+					n=0;
+				}else{
+					toggle=true;
+					col+=n;
+					n=0;
+				}
+
+			}else if(col+n < 0 || col+n > terrainGrid.length ){
+				if(toggle){
+					toggle=false;
+				}else{
+					toggle=true;
+				}
+			}
+
+		}
+		return terrainGrid;
+	}
+
+	private static Tile[][] verticalRoadGenerator(Tile[][] terrainGrid){
+
+		int max = (int)(Math.random()*3) +1, min = (int)(Math.random()*-3)-1; 
+		int n = 1,w = 0, col = (int)(Math.random()*terrainGrid.length);
+		boolean toggle=true;
+
+
+
+		for(int j = 0; j < terrainGrid[col].length; j++){
+			int ran = (int)(Math.random()*4) +3;
+
+			if(w != 0){
+				w+=1;
+				if(w==ran){
+					w=0;
+				}
+
+			}else if(col+n == col+max && toggle && col+n >= 0 && col+n < terrainGrid.length){
+				toggle = false;
+				col+=n;
+				n=0;
+
+			}else if(col+n == col+min && !toggle && col+n >= 0 && col+n < terrainGrid.length){
+				toggle = true;
+				col+=n;
+				n=0;
+
+			}else if(col + n <= col + max && toggle){
+				n++;
+
+			}else if(col + n >= col + min && !toggle){
+				n--;
+
+			}else{
+
+			}
+
+			if( col+n > 0 && col+n < terrainGrid.length-1 ){
+				if(terrainGrid[col+n][j].equals(water)){
+					terrainGrid[col+n][col+n] = bridge;
+				}else{
+					terrainGrid[col+n][j] = road;
+				}
+			}else if(col+n == 0 || col+n == terrainGrid.length-1){
+				if(terrainGrid[col+n][j].equals(water)){
+					terrainGrid[col+n][j] = bridge;
+				}else{
+					terrainGrid[col+n][j] = road;
+				}
+				if(toggle){
+					toggle=false;
+					col+=n;
+					n=0;
+				}else{
+					toggle=true;
+					col+=n;
+					n=0;
+				}
+
+			}else if(col+n < 0 || col+n > terrainGrid.length ){
+				if(toggle){
+					toggle=false;
+				}else{
+					toggle=true;
+				}
+			}
+
+		}
+		return terrainGrid;
+	}
+
+	private static Tile[][] horizontalRoadGenerator(Tile[][] terrainGrid){
+
+		int max = (int)(Math.random()*3) +1, min = (int)(Math.random()*-3)-1; 
+		int n = 1,w = 0, row = (int)(Math.random()*terrainGrid[0].length);
+
+		boolean toggle=true;
+
+		for(int i =0; i < terrainGrid.length; i++){
+			int ran = (int)(Math.random()*4) +3;
+
+			if(w != 0){
+				w+=1;
+				if(w==ran){
+					w=0;
+				}
+			}else if(row+n == row+max && toggle && row+n >= 0 && row+n < terrainGrid[i].length){
+				toggle = false;
+				row+=n;
+				n=0;
+
+			}else if(row+n == row+min && !toggle && row+n >= 0 && row+n < terrainGrid[i].length){
+				toggle = true;
+				row+=n;
+				n=0;
+
+			}else if(row + n <= row + max && toggle){
+				n++;
+
+			}else if(row + n >= row + min && !toggle){
+				n--;
+
+			}else{
+
+			}
+
+			if( row+n > 0 && row+n < terrainGrid[i].length-1 ){
+				if(terrainGrid[i][row+n].equals(water)){
+					terrainGrid[i][row+n] = bridge;
+				}else{
+					terrainGrid[i][row+n] = road;
+				}
+			}else if(row+n == 0 || row+n == terrainGrid[i].length-1){
+				if(terrainGrid[i][row+n].equals(water)){
+					terrainGrid[i][row+n] = bridge;
+				}else{
+					terrainGrid[i][row+n] = road;
+				}
+				if(toggle){
+					toggle=false;
+					row+=n;
+					n=0;
+				}else{
+					toggle=true;
+					row+=n;
+					n=0;
+				}
+
+			}else if(row+n < 0 || row+n > terrainGrid[i].length ){
+				if(toggle){
+					toggle=false;
+				}else{
+					toggle=true;
+				}
+			}
+		}
+
+		return terrainGrid;
+	}
+
+	public Tile[][] getTerrainGrid(){
+		return terrainGrid;
+	}
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				JFrame frame = new JFrame("Game");
+				MapGUI map = new MapGUI();
+				frame.add(map);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.pack();
+				frame.setVisible(true);
+			
+			}
+		});
+
+
+	}
+
+}
