9fe272dc6806bc50ffa2774b311d66b579d42262
Nickolas Jensen <jensenn@student.psdr3.org>
2018-05-03 11:00:28 -0500

    MapVisualizer (hopefully) finalized (or very close)

--- src/view/MapVisualizer.java
+++ src/view/MapVisualizer.java
@@ -6,27 +6,30 @@ import java.awt.*;
 
 public class MapVisualizer extends JPanel implements Tilesets {
 	public static int gridPixelSize= 10;
-	public static World w1 = new World("Map1");
-	
-	
-	
+	public static World w1 = new World("Map1",20,grassGenList);
+	private static Tile[][] currentMap;
+
+
+
 	public MapVisualizer(World w) {
-		Tile[][] currentMap = w.getMap();
+		w.generate();
+		currentMap = w.getMap();
 		for(int i = 0; i < currentMap.length; i++){
 			for(int j = 0; j < currentMap[i].length; j++){
 				System.out.println(currentMap[i][j]);
 			}
 			System.out.println("\n");
 		}
+
 	}
-	
+
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		World currentMap = w1;
+		Tile[][] currentMap = this.currentMap;
 		g.clearRect(0, 0, getWidth(), getHeight());
-		int size = getWidth() / currentMap.getSize();
-		for (int i = 0; i < currentMap.getSize(); i++) {
-			for (int j = 0; j < currentMap.getSize(); j++) {
+		int size = getWidth() / currentMap.length;
+		for (int i = 0; i < currentMap.length; i++) {
+			for (int j = 0; j < currentMap.length; j++) {
 				int x = i * size;
 				int y = j * size;
 				Color terrainColor = w1.getMap()[i][j].getColor();
@@ -34,11 +37,11 @@ public class MapVisualizer extends JPanel implements Tilesets {
 				g.fillRect(x, y, size, size);
 			}
 		}
-		
+
 	}
 
-public static void main(String[] args) {
-		
+	/* public static void main(String[] args) {
+
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				JFrame frame = new JFrame("Game");
@@ -47,10 +50,11 @@ public static void main(String[] args) {
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.pack();
 				frame.setVisible(true);
-			
+
 			}
 		});
 
 
-	}
+	} */
+
 }
